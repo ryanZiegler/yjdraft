c2780af02a018a8be132edbfefd5c933a07032c8
Ego <tonglib@yonyou.com>
2019-04-03 17:45:06 +0800

    chore(sort): add bucketSort

--- base/sort.js
+++ base/sort.js
@@ -134,7 +134,7 @@ console.log('归并排序: ', mergeArr);
 function quickSort(arr, left, right) {
     if (!Array.isArray(arr) || !arr.length) return console.log('参数非法');
     if (!arr.every(e => { return !isNaN(e) })) return console.log('参数对象非法');
-    
+
     let len = arr.length,
         partitionIndex;
     left = typeof left != 'number' ? 0 : left,
@@ -170,3 +170,119 @@ function swap(arr, i, index) {
 const quickArr = [6,3,8,2,9,1];
 quickSort(quickArr);
 console.log('快速排序: ', quickArr);
+
+// 堆排序 (大顶堆, 小顶堆)
+// 1.建立大顶堆  2.堆头,堆尾互换, len--  3.调整大顶堆
+function heapSort(arr) {
+    if (!Array.isArray(arr) || !arr.length) return console.log('参数非法');
+    if (!arr.every(e => { return !isNaN(e) })) return console.log('参数对象非法');
+
+    let len = arr.length;
+    buildMaxHeap(arr);                          // 1
+
+    for (let i = len - 1; i > 0; i--) {         
+        swap(arr, 0, i);                        // 2
+        len--;
+        heapify(arr, 0);                        // 3
+    }
+    return arr;
+
+    function buildMaxHeap(arr) {
+        for (let i = Math.floor(len / 2); i >= 0; i--) {
+            heapify(arr, i);
+        }
+    }
+
+    function heapify(arr, i) {
+        let left = 2 * i + 1,
+            right = 2* i + 2,
+            largest = i;
+
+        if (left < len && arr[largest] < arr[left]) {
+            largest = left;
+        }
+
+        if (right < len && arr[largest] < arr[right]) {
+            largest = right;
+        }
+
+        if (largest !== i) {
+            swap(arr, i, largest);
+            heapify(arr, largest);
+        }
+    }
+}
+
+const heapArr = [6,3,8,2,9,1];
+heapSort(heapArr);
+console.log('堆排序: ', heapArr);
+
+// 计数排序
+function countingSort(arr) {
+    if (!Array.isArray(arr) || !arr.length) return console.log('参数非法');
+    if (!arr.every(e => { return !isNaN(e) })) return console.log('参数对象非法');
+
+    let maxValue = Math.max(...arr),
+        sortedIndex = 0,
+        bucket = new Array(maxValue + 1),
+        bucketLen = maxValue + 1,
+        len = arr.length;
+    
+    for (let i = 0; i < len; i++) {
+        if (!bucket[arr[i]]) {
+            bucket[arr[i]] = 0;
+        }
+        bucket[arr[i]]++;
+    }
+
+    for (let j = 0; j < bucketLen; j++) {
+        while(bucket[j] > 0) {
+            arr[sortedIndex++] = j;
+            bucket[j]--;
+        }
+    }
+
+    return arr;
+}
+
+const countingArr = [6,3,8,2,9,1];
+countingSort(countingArr);
+console.log('计数排序: ', countingArr);
+
+// 桶排序(箱排序)
+function bucketSort(arr, bucketSize) {
+    if (!Array.isArray(arr) || !arr.length) return console.log('参数非法');
+    if (!arr.every(e => { return !isNaN(e) })) return console.log('参数对象非法');
+
+    const maxValue = Math.max(...arr),
+          minValue = Math.min(...arr),
+          DEFAULT_BUCKET_SIZE = 3,
+          len = arr.length;
+
+    bucketSize = bucketSize || DEFAULT_BUCKET_SIZE;
+    const bucketCount = Math.floor((maxValue - minValue) / bucketSize) + 1;
+    const bucket = new Array(bucketCount);
+    for (let i = 0; i < bucketCount; i++) {             // 人造桶
+        bucket[i] = [];
+    }
+
+    // 利用映射函数将数据分配到各个桶中
+    for (let i = 0; i < len; i++) {
+        bucket[Math.floor((arr[i] - minValue) / bucketSize)].push(arr[i]);
+    }
+
+    arr.length = 0;
+    for (let j = 0; j < bucket.length; j++) {
+        if (!bucket[j].length) continue;                // 空桶滚蛋
+        insertionSort(bucket[j]);                       // 插入排序
+        for (let k = 0; k < bucket[j].length; k++) {
+            arr.push(bucket[j][k]);
+        }
+    }
+
+    return arr;
+}
+
+const bucketArr = [63, 157, 189, 51, 101, 47, 141, 121, 157, 156, 194];
+bucketSort(bucketArr);
+console.log('桶排序: ', bucketArr);
