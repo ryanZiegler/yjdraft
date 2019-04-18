302dd9db5ab16a139e6ebb6392ab6dde014f92c3
Ego <tonglib@yonyou.com>
2019-04-18 16:59:43 +0800

    chore(base): add myDescartes

---
+++ base/myDescartes.js
@@ -0,0 +1,13 @@
+function myDescartes() {
+    return Array.prototype.reduce.call(arguments, function(a, b) {
+        var ret = [];
+        a.forEach(a => {
+            b.forEach(b => {
+                ret.push(a.concat([b]));
+            })
+        });
+        return ret;
+    }, [[]]);
+}
+
+console.log(myDescartes(['1', '8'], ['a', 'c'], ['x', 'y']));
