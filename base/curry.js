95305fc440ce1938ea659f63324f1a68747fbcdc
Ego <tonglib@yonyou.com>
2019-05-31 11:51:02 +0800

    feat(curry): add add question

--- base/curry.js
+++ base/curry.js
@@ -13,3 +13,27 @@ function curry(func) {
         return func.apply(null, finalArgs);
     }
 }
+
+/**
+ *  add(1); 	    // 1
+ *  add(1)(2);  	// 3
+ *  add(1)(2)(3)；  // 6
+ *  add(1)(2, 3);   // 6
+ *  add(1, 2)(3);   // 6
+ *  add(1, 2, 3);   // 6 
+ */
+function add() {
+    let args = [].slice.call(arguments);
+    let fn = function(){
+        let fn_args = [].slice.call(arguments)
+        return add.apply(null,args.concat(fn_args))
+    }
+  
+    fn.toString = function(){
+        return args.reduce((a,b)=>a+b)
+    }
+    
+    return fn;
+}
+
+  console.log(add(1,2)(3)(4).toString());
