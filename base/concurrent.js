3f094eafdb1eb257ee3cfb8a4e5c553186b63212
Ego <tonglib@yonyou.com>
2019-09-19 15:36:02 +0800

    feat(practice): add concurrently

---
+++ base/concurrent.js
@@ -0,0 +1,46 @@
+import { resolve } from "dns";
+import { reject } from "async";
+
+class Concurrently {
+    constructor(taskLimit) {
+        if (taskLimit < 0) {
+            throw new Error('Limit cant be lower than 0');
+        }
+        this.tasksQueue = [];
+        this.taskLimit = taskLimit;
+        this.tasksActiveAmount = 0;
+    }
+
+    registerTask(handler) {
+        this.tasksQueue = [...this.tasksQueue, handler];
+        this.executeTasks();
+    }
+
+    executeTasks() {
+        while (this.tasksQueue.length && this.tasksActiveAmount < this.taskLimit) {
+            const task = this.tasksQueue[0];
+            this.tasksQueue = this.tasksQueue.slice(1);
+            this.tasksActiveAmount += 1;
+
+            task().then((result) => {
+                this.tasksActiveAmount -= 1;
+                this.executeTasks();
+
+                return result;
+            }).catch((err) => {
+                this.tasksActiveAmount -= 1;
+                this,this.executeTasks();
+
+                throw err;
+            });
+        }
+    }
+
+    task(handler) {
+        return new Promise((resolve, reject) => {
+            this.registerTask(() => handler().then(resolve).catch(reject))
+        });
+    }
+}
+
+export default Concurrently;
